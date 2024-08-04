9581ee7abc6c3ad50465b49311b7ab18e7b7c64c
Mafaz03 <mohdmafaz200303@gmail.com>
2024-08-04 14:02:52 +0530

    time fetch almost complete

--- internal/timeInfo/timeInfo.go
+++ internal/timeInfo/timeInfo.go
@@ -4,39 +4,64 @@ import (
 	"encoding/json"
 	"errors"
 	"os"
+	"time"
 )
 
-type timeInfo struct {
-	Static_info string `json:"static_info"`
-	Last_run_time string `json:"last_run_time"`
+type TimeInfo struct {
+	StaticInfo        string    `json:"static_info"`
+	LastRunTime       string    `json:"last_run_time"`
+	LastRunTimeParsed time.Time `json:"-"`
 }
 
-func readData(filename string) (*timeInfo, error) {
-    encData, err := os.ReadFile(filename)
-    if err != nil {
-        return nil, errors.New("time related json file could not be read")
-    }
+func ReadData(filename string) (*TimeInfo, error) {
+	encData, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, errors.New("time related json file could not be read")
+	}
 
-    timeinfo := &timeInfo{}
-    err = json.Unmarshal(encData, timeinfo)
-    if err != nil {
-        return nil, errors.New("error unmarshaling json data")
-    }
+	timeinfo := &TimeInfo{}
+	err = json.Unmarshal(encData, timeinfo)
+	if err != nil {
+		return nil, errors.New("error unmarshaling json data")
+	}
 
-    return timeinfo, nil
+	if timeinfo.LastRunTime != "" {
+		timeinfo.LastRunTimeParsed, err = time.Parse("2006-01-02 15:04:05", timeinfo.LastRunTime)
+		if err != nil {
+			return nil, errors.New("error parsing last run time")
+		}
+	}
+
+	return timeinfo, nil
 }
 
-func writeData(filename string, data *timeInfo) error {
+func WriteData(filename string, data *TimeInfo) error {
 
 	encData, err := json.MarshalIndent(data, "", " ")
 	if err != nil {
-        return err
-    }
+		return err
+	}
 
-    err = os.WriteFile(filename, encData, 0644)
-    if err != nil {
-        return errors.New("time related json file could not be saved")
-    }
+	err = os.WriteFile(filename, encData, 0644)
+	if err != nil {
+		return errors.New("time related json file could not be saved")
+	}
 
-    return nil
+	return nil
+}
+
+func UpdateLastRunTime(filename string) error {
+	data, err := ReadData(filename)
+	if err != nil {
+		return err
+	}
+
+	data.LastRunTimeParsed = time.Now().UTC()
+	data.LastRunTime = data.LastRunTimeParsed.Format("2006-01-02 15:04:05")
+	err = WriteData(filename, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -1,36 +1,22 @@
 package main
 
 import (
-	"encoding/json"
 	"encoding/xml"
-	"errors"
 	"fmt"
 	"log"
-	"os"
 	"time"
 
 	"github.com/Mafaz03/arxivAPI/internal/arxivapi"
 	"github.com/Mafaz03/arxivAPI/internal/timeinfo"
 )
 
-
-func main() {
-	
-	data, err := timeinfo.readData("timeInfo.json")
-	if err != nil {
-        fmt.Println(err)
-    }
-    fmt.Println(data.Last_run_time)
-	data.Last_run_time = time.Now().Format("2006-01-02 15:04:05")
-	writeData("timeInfo.json", data)
-	fmt.Println(data.Last_run_time)
-
+func fetch() {
 	client := arxivapi.NewClient(time.Minute)
 	xmlData := client.FetchPapers()
 
 	x := arxivapi.Feed{}
 
-	err = xml.Unmarshal([]byte(xmlData), &x)
+	err := xml.Unmarshal([]byte(xmlData), &x)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -38,7 +24,35 @@ func main() {
 		// fmt.Println("Empty")
 		return
 	}
+
 	// for i := range len(x.Entry) {
 	// 	// fmt.Println(x.Entry[i].Title + "\n")
 	// }
+
+	for i := range 5 {
+		fmt.Println(x.Entry[i].Title + "\n\n")
+	}
+}
+
+func main() {
+
+	data, err := timeinfo.ReadData("timeInfo.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	now_time := time.Now().UTC()
+
+	diff := now_time.Sub(data.LastRunTimeParsed)
+	if diff > time.Hour {
+		fmt.Println("Updating the times (its been over 1 hour(s) since last update)")
+		err = timeinfo.UpdateLastRunTime("timeInfo.json")
+		if err != nil {
+			fmt.Println("Error updating last run time:", err)
+			return
+		}
+	}
+
+	fetch()
+
 }
